c92d9e4aff562e1f3e3cdaf0ba94036e83604ee6
jun <879987322@qq.com>
2024-08-31 00:41:11 +0800

    组件v-model

---
+++ Vue3/03_组件化开发/my-app/src/05_组件v-model/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <h2>你好啊！</h2>
+    <Person />
+  </div>
+</template>
+
+<script setup>
+import Person from './Person.vue';
+</script>
+
+<style scoped>
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px
+  }
+</style>
