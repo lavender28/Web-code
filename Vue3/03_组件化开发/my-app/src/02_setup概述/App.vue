8b728ca812cbcd5a847b6ed1493c17cbfd1b9b45
jun <879987322@qq.com>
2024-08-30 14:33:28 +0800

    setup基本使用

---
+++ Vue3/03_组件化开发/my-app/src/02_setup概述/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <h2>你好啊！</h2>
+    <Person />
+  </div>
+</template>
+
+<script>
+import Person from './Person.vue';
+
+export default {
+  components: {
+    Person
+  }
+}
+</script>
+
+<style scoped>
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px
+  }
+</style>
---
+++ Vue3/03_组件化开发/my-app/src/02_setup概述/Person.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="addAge">年龄+1</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<!-- setup 语法糖 -->
+<script setup>
+  // 定义数据
+  let name = '张三';
+  let age = 18;
+  let tel = '13812345678';
+  // 定义方法
+  function addAge() {
+    age += 1;
+  }
+  function showTel() {
+    alert(tel);
+  }
+</script>
+
+<!-- <script>
+
+export default {
+  name: 'Person',
+  setup() {
+    // 定义数据
+    let name = '张三';
+    let age = 18;
+    let tel = '13812345678';
+    // 定义方法
+    function addAge() {
+      age += 1;
+    }
+    function showTel() {
+      alert(tel);
+    }
+    // 返回要暴露给模板的数据和方法
+    return { name, age, addAge, showTel }
+  }
+}
+</script> -->
+
+<style scoped>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+  button {
+    margin: 0 5px;
+    background-color: #4CAF50;
+    color: white;
+  }
+</style>
