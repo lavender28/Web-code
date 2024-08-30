24b2511bf76f4701a1088b7d5eebd8d8bf4bd55d
jun <879987322@qq.com>
2024-08-30 14:57:15 +0800

    增加ref和reactive

--- Vue3/03_组件化开发/my-app/src/02_setup概述/Person.vue
+++ Vue3/03_组件化开发/my-app/src/02_setup概述/Person.vue
@@ -4,21 +4,41 @@
     <h2>年龄：{{ age }}</h2>
     <button @click="addAge">年龄+1</button>
     <button @click="showTel">查看联系方式</button>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <p>职业：{{ person.job.title }}</p>
+    <p>工作年限：{{ person.job.years }}</p>
+    <button @click="addWorkYear">工作年限+1</button>
   </div>
 </template>
 
 <!-- setup 语法糖 -->
 <script setup>
+  import { ref, reactive } from 'vue'
   // 定义数据
   let name = '张三';
-  let age = 18;
+  let age = ref(18);   // 使用 ref 定义响应式数据
   let tel = '13812345678';
+
+  // 使用 reactive 定义响应式数据
+  let person = reactive({
+    name: '李四',
+    age: 20,
+    job: {
+      title: '前端开发',
+      years: 1
+    }
+  })
   // 定义方法
   function addAge() {
-    age += 1;
+    age.value += 1;  // 使用 .value 访问 ref 的值
   }
   function showTel() {
     alert(tel);
+  }
+  function addWorkYear() {
+    // 修改嵌套的响应式数据
+    person.job.years++
   }
 </script>
 
