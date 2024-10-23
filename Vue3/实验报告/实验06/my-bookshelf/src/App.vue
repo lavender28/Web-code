cf897144c8fd1ff3366f1e4735fd0bd1b69e49ef
jun <879987322@qq.com>
2024-10-23 10:56:53 +0800

    实验6内容 父子组件通信

---
+++ Vue3/实验报告/实验06/my-bookshelf/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BookShelf from './components/BookShelf.vue';
+</script>
+
+<template>
+  <div class="my-bookshelf">
+    <h1>我的书架</h1>
+    <BookShelf />
+  </div>
+</template>
+
+<style>
+.my-bookshelf {
+  font-family: Arial, Helvetica, sans-serif;
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+  background-color: #f0f4f8;
+  border-radius: 10px;
+  box-shadow: 0 0 10px raba(0, 0, 0, 0.5);
+}
+h1 {
+  color: #2c3e50;
+  text-align: center;
+}
+</style>
---
+++ Vue3/实验报告/实验06/my-bookshelf/src/components/Rating.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  rating: {
+    type: Number,
+    Required: true
+  }
+});
+
+const emit = defineEmits(['update-rating']);
+
+const setRating = (newRating) => {
+  emit('update-rating', newRating);
+}
+</script>
+
+<template>
+  <div class="rating">
+    <span 
+      v-for="star in 5"
+      :key="star"
+      @click="setRating(star)"
+      class="star"
+      :class="{filled: star <= rating}">★
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.star {
+  cursor: pointer;
+  font-size: 20px;
+  color: #ccc;
+}
+.star.filled {
+  /* color: #f39c12; */
+  color: gold;
+}
+</style>
