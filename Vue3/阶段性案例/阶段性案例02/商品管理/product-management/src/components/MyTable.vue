8ee66d5f54f6f1d115dd707de119f38b435fad78
jun <879987322@qq.com>
2024-11-08 21:24:12 +0800

    阶段案例2-商品管理

---
+++ Vue3/阶段性案例/阶段性案例02/商品管理/product-management/src/components/MyTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <table class="table table-striped table-bordered">
+    <!-- 表格标题区域 -->
+    <thead >
+      <tr>
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <!-- 表格主体区域 -->
+    <tbody>
+      <tr v-for="(item, index) in goodsList" :key="item.id">
+        <slot name="body" :row="item" :index="index"></slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+const props = defineProps({
+  goodsList: {
+    type: Array,
+    required: true,
+    default: []
+  }
+})
+</script>
